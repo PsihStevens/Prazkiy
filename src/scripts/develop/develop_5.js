ab39f319ab1262c385e15656c051f7163a2a1f9f
Artem Lopatovskiy <herzz86@gmail.com>
2017-05-21 02:55:06 +0300

    section 1, 2, 3, 4, 5, 6, 7, 8, 11

--- src/scripts/develop/develop_5.js
+++ src/scripts/develop/develop_5.js
@@ -1,7 +1,68 @@
 
+function owlCarousel() {
 
+    $('#slider-header').owlCarousel({
+        items: 1,
+        singleItem: true,
+        slideSpeed: 1000,
+        navigation: true,
+        mouseDrag: true,
+        pagination: false,
+        autoplay: true,
+        loop: true,
+        smartSpeed: 2000,
+        autoplayTimeout: 5000,
+        // afterAction: syncPosition1,
+        responsiveRefreshRate: 200,
+        navigationText: [
+            "<span></span>",
+            "<span></span>"
+        ],
+    });
+
+    $('#carusel-full').owlCarousel({
+        items: 1,
+        singleItem: true,
+        navigation: true,
+        mouseDrag: false,
+        pagination: false,
+        // afterAction: syncPosition1,
+        responsiveRefreshRate: 200,
+        navigationText: [
+            "<span></span>",
+            "<span></span>"
+        ],
+    });
+
+    $('#carusel-small').owlCarousel({
+        items: 3,
+        singleItem: true,
+        slideSpeed: 1000,
+        navigation: true,
+        mouseDrag: false,
+        pagination: false,
+        // afterAction: syncPosition1,
+        responsiveRefreshRate: 200,
+        navText: ["",""
+        ],
+        onInitialized: function(event) {
+
+            $('#carusel-small').find(".owl-item").eq(0).addClass("active-slide");
+        }
+    });
+    $("#carusel-small").on("click", ".owl-item", function(event) {
+
+        event.preventDefault();
+        $(".owl-item").removeClass("active-slide");
+        $(this).addClass("active-slide");
+        var number = $(this).index();
+        $('#carusel-full').trigger("to.owl.carousel", number);
+    });
+}
 
 $(document).ready(function(){
+
+    owlCarousel();
 
 });
 
